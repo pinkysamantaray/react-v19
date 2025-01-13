6edd0613bb8f4704bb0d27cbf4897237e340c96b
pinsamantaray <pinsamantaray@deloitte.nl>
2025-01-13 21:58:37 +0100

    Added Testing of components with Vitest, v5

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -4,6 +4,7 @@ const Pizza = (props) => {
       <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
+      {/*  alt={"Testing Test case"} Add and see the test case failing */}
     </div>
   );
 };
